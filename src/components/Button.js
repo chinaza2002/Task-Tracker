6bbecac131d91e622ca3beda85491823f035a2a2
chinaza2002 <115018232+chinaza2002@users.noreply.github.com>
2023-07-27 17:37:32 +0100

    Add files via upload

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+const Button = (props) => {
+    
+  return (
+    <button 
+    onClick={props.onClick} 
+    className='btn' 
+    style={{ backgroundColor: props.color}}
+    >
+        {props.text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes ={
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func
+}
+
+export default Button
